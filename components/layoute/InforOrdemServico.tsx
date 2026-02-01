8e32bd5de6ee03c370c40efb905fdf574d413fd3
Fernando <fernandoeqp59@gmail.com>
2026-02-01 18:16:50 -0300

    ajustado deploy

--- components/layoute/InforOrdemServico.tsx
+++ components/layoute/InforOrdemServico.tsx
@@ -5,7 +5,6 @@ import {
   DialogTitle,
   DialogTrigger,
 } from "@/components/ui/dialog";
-import { Badge } from "@/components/ui/badge";
 import { Separator } from "@/components/ui/separator";
 import {
   Calendar,
